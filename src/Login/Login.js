f88b80bc97ea32f97ef825ed255ea9f12a29b4f3
minsung521 <mingsung521@gmail.com>
2022-01-05 04:25:36 +0900

    login page responsive css finished

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -7,30 +7,48 @@ function Login() {
 		<Fragment id="login-page">
 			<div className="container">
 				<div className="row">
-					<div className="col">
+					<div className="col colTitle">
 						<div className="title-box">
 							<div className="big-title">Caroro-on</div>
 							<div className="small-title">내 손안의 작은 환경지킴이</div>
 						</div>
 					</div>
-					<div className="col">
-						<div className="imgFrame">
-							<img className="logoImg" src={CaroroLogo} alt="caroro-logo" />
-						</div>
+					<div className="col colImg">
+						<img className="bigLogoImg" src={CaroroLogo} alt="caroro-logo" />
+
 						<ul className="input-wrap">
 							<li>
 								<div className="input-title">카로로에 로그인하세요</div>
 							</li>
 							<form action="">
 								<li>
-									<input type="text" />
+									<input
+										id="email"
+										name="email"
+										type="text"
+										className="input inputText"
+										placeholder="이메일"
+									/>
 								</li>
 								<li>
-									<input type="text" />
+									<input
+										id="password"
+										name="password"
+										type="text"
+										className="input inputText"
+										placeholder="비밀번호"
+									/>
 								</li>
 								<li>
-									<button className="submit">
-										<span className="btn-text">로그인</span>
+									<div className="input-linkText">
+										계정이 없나요?
+										<a href=""> 회원가입 </a>
+										하기
+									</div>
+								</li>
+								<li>
+									<button className="input inputBtn" type="submit">
+										로그인
 									</button>
 								</li>
 							</form>
