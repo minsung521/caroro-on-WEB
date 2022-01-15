6dc25f9ad5d01d748c5ef4158d1da07bc60c6381
minsung521 <mingsung521@gmail.com>
2022-01-15 14:04:12 +0900

    why doesn't work?

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,5 +1,6 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import "./Main.scss";
+import Slider from "react-slick";
 import caroroLogo from "../assets/caroro-small-topright.svg";
 import userProfileImg from "../store/test/userProfileImg.png";
 
@@ -41,7 +42,56 @@ function Main() {
 		},
 	]);
 
-	const setting = [];
+	const [activeSlide, setActiveSlide] = useState(0);
+
+	// const settings = {
+	// 	dots: true,
+	// 	infinite: false,
+	// 	speed: 1000,
+	// 	slidesToShow: 1,
+	// 	slidesToScroll: 1,
+	// 	// beforeChange: (current, next) => this.setState({ activeSlide: next }),
+	// 	beforeChange: (current, next) => setActiveSlide(next),
+	// };
+
+	const settings = {
+		dots: true,
+		infinite: false,
+		speed: 500,
+		slidesToShow: 4,
+		slidesToScroll: 4,
+		initialSlide: 0,
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3,
+					infinite: true,
+					dots: true,
+				},
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2,
+					initialSlide: 2,
+				},
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+				},
+			},
+		],
+	};
+
+	useEffect(() => {
+		console.log(activeSlide);
+	}, [activeSlide]);
 
 	return (
 		<>
@@ -64,6 +114,33 @@ function Main() {
 				</div>
 				<div className="main-container__row">
 					<img src="" alt="" />
+					<div className="test slide">
+						<h2>beforeChange and afterChange hooks</h2>
+						<p>
+							<strong>{activeSlide}</strong>
+						</p>
+
+						<Slider {...settings}>
+							<div className="test">
+								<h3>1</h3>
+							</div>
+							<div className="test">
+								<h3>2</h3>
+							</div>
+							<div className="test">
+								<h3>3</h3>
+							</div>
+							<div className="test">
+								<h3>4</h3>
+							</div>
+							<div className="test">
+								<h3>5</h3>
+							</div>
+							<div className="test">
+								<h3>6</h3>
+							</div>
+						</Slider>
+					</div>
 				</div>
 			</div>
 		</>
